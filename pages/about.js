a88084063c8bfbedb293f7a0cf720253ba08fc83
Ceyda <ceydaonemli2@gmail.com>
2023-01-03 18:45:26 +0300

    router and pages

---
+++ pages/about.js
@@ -0,0 +1,21 @@
+const About = () => {
+  return (
+    <div>
+      <h1>About Page</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias
+        corrupti vero dolorem quos esse praesentium illo rerum blanditiis
+        nostrum! Quibusdam qui natus nulla. Delectus tenetur omnis magnam
+        recusandae qui officiis?
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias
+        corrupti vero dolorem quos esse praesentium illo rerum blanditiis
+        nostrum! Quibusdam qui natus nulla. Delectus tenetur omnis magnam
+        recusandae qui officiis?
+      </p>
+    </div>
+  )
+}
+
+export default About
