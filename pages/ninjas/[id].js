1ca2c8aaa998884c87be448dc8d751e66f97207f
Ceyda <ceydaonemli2@gmail.com>
2023-01-04 22:28:01 +0300

    dynamic routes

--- pages/ninjas/[id].js
+++ pages/ninjas/[id].js
@@ -1,21 +1,36 @@
-export const getStaticPaths=async()=>{
-    const res=await fetch("https://jsonplaceholder.typicode.com/users")
-    const data=await res.json();
+export const getStaticPaths = async () => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users")
+  const data = await res.json()
 
-    const paths=data.map(ninja=>{
-        return {
-            params:{id:ninja.id.toString()}
-        }
-    }) 
+  const paths = data.map((ninja) => {
     return {
-       paths,
-       fallback:false
-      }
+      params: { id: ninja.id.toString() },
+    }
+  })
+  return {
+    paths,
+    fallback: false,
+  }
 }
-const Details = () => {
-    return ( <div>
-        <h1>Details Page</h1>
-    </div> );
+export const getStaticProps = async (context) => {
+  const id = context.params.id
+  const res = await fetch("https://jsonplaceholder.typicode.com/users/" + id)
+  const data = await res.json()
+  return {
+    props: {
+      ninja: data,
+    },
+  }
 }
- 
-export default Details;
+const Details = ({ninja}) => {
+  return (
+    <div>
+      <h1>{ninja.name}</h1>
+      <p>{ninja.email}</p>
+      <p>{ninja.website}</p>
+      <p>{ninja.address.city}</p>
+    </div>
+  )
+}
+
+export default Details
