29948e4a69d282b495555bb5d725974afe30bf55
Ceyda <ceydaonemli2@gmail.com>
2023-01-04 18:04:11 +0300

    components and styles added

---
+++ comps/Footer.js
@@ -0,0 +1,7 @@
+const Footer = () => {
+    return (<footer>
+        Copyright 2022 Ninja List
+    </footer>);
+}
+ 
+export default Footer;
--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,25 @@
 import Head from "next/head"
 import styles from "../styles/Home.module.css"
 
+import Link from "next/link"
 export default function Home() {
   return (
     <div>
-      <h1>Homepage</h1>
-      <p>
+      <h1 className={styles.title}>Homepage</h1>
+      <p className={styles.text}>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos laborum
         voluptatem, deleniti corporis eos provident aliquid qui ea maiores sed
         ut sint tempora officiis earum alias exercitationem, minus, id
         aspernatur.
       </p>
-      <p>
+      <p className={styles.text}>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus
         explicabo accusantium laborum repudiandae sint voluptatum architecto
         atque incidunt tenetur animi, eligendi quas nemo voluptatibus ratione
         dicta perspiciatis doloremque aperiam temporibus.
       </p>
+      <Link href="/ninjas"><a className={styles.btn}>See Ninjas Listing</a></Link>
+    
     </div>
   )
 }
