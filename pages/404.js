8a6267624c120dd4cf179f420fda6615c17b11b7
Ceyda <ceydaonemli2@gmail.com>
2023-01-04 19:27:17 +0300

    redirecting users

--- pages/404.js
+++ pages/404.js
@@ -1,5 +1,15 @@
 import Link from "next/link"
+import { useRouter } from "next/router"
+import { useEffect } from "react"
+useRouter
 const NotFound = () => {
+    const router=useRouter();
+    useEffect(()=>{
+        setTimeout(()=>{
+            //router.go(1)
+            router.push("/");//redirect to homepage
+        },3000)
+    },[])
   return (
     <div className="not-found">
       <h1>Opppss..</h1>
